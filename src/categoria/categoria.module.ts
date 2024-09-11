eb4ba7b2c80bb6acdb56367a8852a0649f093fce
Luana Paes <luanafranca598@gmail.com>
2024-09-10 23:24:11 -0300

    feat(categoria): adiciona middleware de verificação de ID de categorias e importa módulos de autenticação e usuários
    - Configura o `CategoriaIdCheckMiddleware` no `CategoriaModule` para verificar o ID do usuario em todas as requisições que usam a rota `categorias/:id`.
    - Define a aplicação do middleware para qualquer método HTTP (`RequestMethod.ALL`) em rotas que envolvam o ID do produto.
    
    Essas mudanças garantem que todas as rotas que lidam com produtos tenham verificação de ID e integram autenticação e controle de usuários nas operações de produtos.

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,14 +1,26 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { forwardRef, MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { CategoriaController } from "./categoria.controller";
 import { CategoriaService } from "./categoria.service";
 import { MarcaModule } from "src/marca/marca.module";
+import { CategoriaIdCheckMiddleware } from "src/middlewares/categoria-id-check.middlware";
+import { AuthModule } from "src/auth/auth.module";
+import { UsuarioModule } from "src/usuario/usuario.module";
 
 @Module({
     imports: [
-        forwardRef(() => MarcaModule)
+        forwardRef(() => MarcaModule),
+        AuthModule, UsuarioModule
     ],
     controllers: [CategoriaController],
     providers: [CategoriaService],
     exports: []
 })
-export class CategoriaModule {}
+export class CategoriaModule {
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(CategoriaIdCheckMiddleware).forRoutes({
+            path: 'categorias/:id',
+            method: RequestMethod.ALL
+        })
+    }
+}
