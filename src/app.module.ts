f3cf43479636123e7105c1b6b07e1143ce964fa4
Luana Paes <luanafranca598@gmail.com>
2024-09-10 22:52:34 -0300

    feat(app): configura módulos e entidades no AppModule com TypeORM e MySQL
    - Adiciona as seguintes importações no `AppModule`:
      - `ConfigModule` para gerenciamento de variáveis de ambiente.
      - `TypeOrmModule` para configuração da conexão com banco de dados MySQL.
      - `UsuarioModule`, `MarcaModule`, `ProdutoModule` e `AuthModule` para estruturar os módulos do projeto.
    - Configura a conexão com o banco de dados MySQL usando `TypeOrmModule.forRoot()`:
      - Utiliza variáveis de ambiente para definir host, porta, usuário, senha e nome do banco de dados.
      - Inclui as entidades `UsuarioEntity`, `MarcaEntity`, `CategoriaEntity` e `ProdutoEntity` para serem gerenciadas pelo TypeORM.
      - Habilita `synchronize` em ambiente de desenvolvimento para sincronização automática do esquema do banco de dados.
    - Atualiza a estrutura do projeto, adicionando o gerenciamento de produtos, marcas, categorias e autenticação.
    
    Essas mudanças integram a conexão com o banco de dados MySQL e estabelecem as entidades e módulos necessários para manipular usuários, marcas, produtos e categorias.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioEntity } from './usuario/entity/usuario.entity';
+import { MarcaEntity } from './marca/entity/marca.entity';
+import { CategoriaEntity } from './categoria/entity/categoria.entity';
+import { ProdutoEntity } from './produto/entity/produto.entity';
+import { ProdutoModule } from './produto/produto.module';
+import { MarcaModule } from './marca/marca.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [UsuarioEntity, MarcaEntity, CategoriaEntity, ProdutoEntity],
+      synchronize: process.env.ENV === "development",
+    }),
+    MarcaModule,
+    UsuarioModule,
+    AuthModule,
+    ProdutoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
