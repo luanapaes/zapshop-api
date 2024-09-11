4cea817f5774f7419f9e2831d915723c7f5e2e17
Luana Paes <luanafranca598@gmail.com>
2024-09-10 23:23:11 -0300

    feat(usuario): adiciona middleware de verificação de ID de produto e importa módulos de autenticação e usuários
    - Configura o `UsuarioIdCheckMiddleware` no `UsuarioModule` para verificar o ID do usuario em todas as requisições que usam a rota `usuarios/:id`.
    - Define a aplicação do middleware para qualquer método HTTP (`RequestMethod.ALL`) em rotas que envolvam o ID do produto.
    
    Essas mudanças garantem que todas as rotas que lidam com produtos tenham verificação de ID e integram autenticação e controle de usuários nas operações de produtos.

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,15 +1,26 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
 import { UsuarioService } from "./usuario.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsuarioEntity } from "./entity/usuario.entity";
+import { UsuarioIdCheckMiddleware } from "src/middlewares/usuario-id-check.middlware";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([UsuarioEntity]),
+        AuthModule
     ],
     controllers: [UsuarioController],
     providers: [UsuarioService, UsuarioEntity],
     exports: [UsuarioService]
 })
-export class UsuarioModule {}
+export class UsuarioModule {
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(UsuarioIdCheckMiddleware).forRoutes({
+            path: 'usuarios/:id',
+            method: RequestMethod.ALL
+        })
+    }
+}
