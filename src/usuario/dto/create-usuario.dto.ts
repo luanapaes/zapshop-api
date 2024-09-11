33f32a90d4f421cec3d7134d8f1f84ebeef7d634
Luana Paes <luanafranca598@gmail.com>
2024-09-10 22:43:04 -0300

    feat(usuario): add UsuarioController

---
+++ src/usuario/dto/create-usuario.dto.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+
+export class CreateUsuarioDTO {
+    
+    @IsString()
+    nome: string;
+
+    @IsString()
+    nome_empresa: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 6,
+        minLowercase: 0,
+        minUppercase: 0,
+        minNumbers: 0,
+        minSymbols: 0,
+    })
+    password: string;
+}
