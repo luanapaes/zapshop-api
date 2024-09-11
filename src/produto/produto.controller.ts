9c7c8f7e36e7f9592d00e05488567153dc784249
Luana Paes <luanafranca598@gmail.com>
2024-09-10 23:19:18 -0300

    feat(produto): adiciona AuthGuard ao ProdutoController para proteção de rotas
    - Adiciona o decorator `@UseGuards(AuthGuard)` no `UsuarioController`, protegendo todas as rotas do controlador com o `AuthGuard`.

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { ProdutoService } from "./produto.service";
 import { CreateProdutoDTO } from "./dto/create-produto.dto";
+import { AuthGuard } from "src/guards/auth.guard";
 
+@UseGuards(AuthGuard)
 @Controller('produtos')
 export class ProdutoController {
     constructor(
