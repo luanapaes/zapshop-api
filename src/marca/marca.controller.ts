405917381bc975babbe46dd787c8800921d5da72
Luana Paes <luanafranca598@gmail.com>
2024-09-12 20:19:55 -0300

    fix(marca): corrige a captura de dados no body e obtém usuarioId da requisição corretamente

--- src/marca/marca.controller.ts
+++ src/marca/marca.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Post, Request, UseGuards } from "@nestjs/common";
 import { MarcaService } from "./marca.service";
 import { CreateMarcaDTO } from "./dto/create-marca.dto";
 import { AuthGuard } from "src/guards/auth.guard";
@@ -11,9 +11,14 @@ export class MarcaController {
     ){}
 
     @Post()
-    async create(marca: CreateMarcaDTO, @Request() req){
+    async create(@Body() marca: CreateMarcaDTO, @Request() req){
+        
+        marca.usuarioId = req.user.id;
+        return await this.marcasService.create(marca);
+    }
 
-        marca.usuarioId = req.usuario.id
-        return this.marcasService.create(marca)
+    @Delete(':id')
+    async delete(id: number){
+        return this.marcasService.delete(id)
     }
 }
