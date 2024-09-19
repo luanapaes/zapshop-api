fac421e14bf03aecc99333c3833ad4a601193d03
Luana Paes <luanafranca598@gmail.com>
2024-09-18 23:14:09 -0300

    feat(produto): add funções read e readOne para listar produtos e adiciona upload de imagem para cadastrar também a imagem do produto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { ProdutoService } from "./produto.service";
 import { CreateProdutoDTO } from "./dto/create-produto.dto";
 import { AuthGuard } from "src/guards/auth.guard";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { UpdatePutProdutoDTO } from "./dto/update-put-produto.dto";
 
 @UseGuards(AuthGuard)
 @Controller('produtos')
@@ -10,8 +12,25 @@ export class ProdutoController {
         private readonly produtoService: ProdutoService
     ){}
 
+    @Get()
+    async read(){
+        return this.produtoService.read();
+    }
+
+    @Get(':id')
+    async readOne(id: number){
+        return this.produtoService.readOne(id);
+    }
+
     @Post()
-    async create(@Body() data: CreateProdutoDTO){
-        return this.produtoService.create(data);
+    @UseInterceptors(FileInterceptor('produto_imagem'))
+    async create(@Body() data: CreateProdutoDTO, @UploadedFile() productImage){
+        return this.produtoService.create(data, productImage);
+    }
+
+    @Put(':id')
+    async update(@Body() produto: UpdatePutProdutoDTO, @Param("id") id: number) {
+        return this.produtoService.update(id, produto);
+
     }
 }
