ed8e0a19aebb1db17b0dc335184ece6ac5aa3942
Luana Paes <luanafranca598@gmail.com>
2024-10-16 20:21:34 -0300

    feat(marca-controller): ajustes nas funções de retorno e proteção por guard
    - Implementada lógica de retorno condicional no método GET():
      - Quando uma query com o nome da marca é passada (ex: `?nome_marca=natura`), são retornados os detalhes completos da marca, incluindo todos os produtos relacionados.
      - Sem query, o retorno é de todas as marcas cadastradas no sistema.
    - Removido o guard global e aplicado apenas nas funções que exigem proteção, permitindo acesso às marcas sem a necessidade de login.

--- src/marca/marca.controller.ts
+++ src/marca/marca.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Req, Request, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req, Request, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { MarcaService } from "./marca.service";
 import { CreateMarcaDTO } from "./dto/create-marca.dto";
 import { AuthGuard } from "src/guards/auth.guard";
@@ -6,17 +6,17 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { UpdatePutMarcaDTO } from "./dto/update-put-marca.dto";
 import { UpdatePatchMarcaDTO } from "./dto/update-patch-marca.dto";
 
-@UseGuards(AuthGuard)
+
 @Controller('marcas')
 export class MarcaController {
     constructor(
         private readonly marcasService: MarcaService
     ){}
 
-    @Get()
-    async read(){
-        return this.marcasService.read()
-    }
+    // @Get()
+    // async read(){
+    //     return this.marcasService.read()
+    // }
 
     // @Get(':id')
     // async listOne(@Param('id') id: number){
@@ -24,10 +24,16 @@ export class MarcaController {
     // }
 
     @Get(':id')
-    async listOne(@Param('id') id: number) {
+    async listByID(@Param('id') id: number) {
         return this.marcasService.getMarcaByIdUsingRelations(id);
     }
 
+    @Get()
+    async listOne(@Query('nome_marca') name: string) {
+        return this.marcasService.findByNome(name);
+    }
+
+    @UseGuards(AuthGuard)
     @Post()
     @UseInterceptors(FileInterceptor('logomarca'))
     async create(@Body() marca: CreateMarcaDTO, @UploadedFile() logomarca, @Request() req){
@@ -35,15 +41,19 @@ export class MarcaController {
         marca.usuarioId = req.user.id;
         return await this.marcasService.create(marca, logomarca);
     }
-
+    
+    @UseGuards(AuthGuard)
     @Put(':id')
-    async update(@Body() marca: UpdatePutMarcaDTO, @Param("id") id: number, @Req() req) {
-        
+    @UseInterceptors(FileInterceptor('logomarca'))
+    async update(@Body() marca: UpdatePutMarcaDTO, @UploadedFile() logomarca, @Param("id") id: number, @Req() req) {
+        console.log(marca.nome_marca)
         marca.usuarioId = req.user.id;
-        return this.marcasService.update(id, marca);
+        marca.logomarca = logomarca
+        return this.marcasService.update(id, marca, logomarca);
 
     }
 
+    @UseGuards(AuthGuard)
     @Patch(':id')
     async updatePartial(@Body() marca: UpdatePatchMarcaDTO, @Param("id") id: number, @Req() req) {
         
@@ -52,6 +62,7 @@ export class MarcaController {
         
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     async delete(id: number){
         return this.marcasService.delete(id)
