ec506fd1a6d8cbd732ce3ded3a34f70fed14ae4a
Luana Paes <luanafranca598@gmail.com>
2024-10-16 20:10:51 -0300

    Habilita CORS para o frontend Angular em localhost e permitir métodos e headers específicos
    - Configura o CORS (Compartilhamento de Recursos de Origem Cruzada) para permitir requisições do frontend Angular em 'http://localhost:4200'.
    - Permitidos métodos HTTP: GET, POST, PATCH, DELETE, OPTIONS, PUT.
    - Define headers específicos que podem ser aceitos, incluindo tokens CSRF, tipo de conteúdo e headers de autorização.
    - Habilita credenciais para permitir o envio de cookies e tokens com as requisições.
    - Expostos os headers 'Access-Control-Allow-Origin' e 'Access-Control-Allow-Credentials' para o frontend, proporcionando maior controle no lado do cliente.

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permitir apenas o front-end do Angular
+    methods: 'GET, POST, PATCH, DELETE, OPTIONS, PUT',
+    allowedHeaders: 'X-CSRF-TOKEN, X-Requested-With, Content-Type, Authorization', // Headers permitidos
+    credentials: true, // Permite o envio de cookies, tokens, etc.
+    exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'], // Headers que podem ser acessados pelo cliente
+  });
 }
 bootstrap();
+
+
