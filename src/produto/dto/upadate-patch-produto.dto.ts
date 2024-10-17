d549b3847334c9025a497731fce83bc530ae0de8
Luana Paes <luanafranca598@gmail.com>
2024-10-16 21:29:02 -0300

    feat(dto): muda nome da classe

--- src/produto/dto/upadate-patch-produto.dto.ts
+++ src/produto/dto/upadate-patch-produto.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreateProdutoDTO } from './create-produto.dto';
 
-export class UpdatePatchMarcaDTO extends PartialType(CreateProdutoDTO) {
+export class UpdatePatchProdutoDTO extends PartialType(CreateProdutoDTO) {
 
 
 }
